257fd5ccd16e444aef3740158299a7c65ce241de
Robert Coe <chakrawc@gmail.com>
2020-01-22 07:01:00 -0800

    rewrite factoryFarm

--- src/sheep/factoryFarm.ts
+++ src/sheep/factoryFarm.ts
@@ -2,21 +2,16 @@
 import { addScriptHook, W3TS_HOOK } from "w3ts";
 import { saveskills } from "shared";
 
-// globals from FactoryFarm:
-const factoryFarmTiemr = CreateTimer();
+const factoryFarmTimer = CreateTimer();
 let factoryFarmDummySheep: unit;
 const factoryFarms = CreateGroup();
-const factoryFarmData = InitHashtable();
 const factoryFarmsTemp = CreateGroup();
 let factoryFarmBuilds: number;
-// endglobals from FactoryFarm
 
 const FACTORY_FARM_TYPE = FourCC( "h00C" );
-const s__FactoryFarm__data_typeIndex = 0;
-const s__FactoryFarm__data_spiralIndex = 1;
-const s__FactoryFarm__data_waitBetweenTicks = 2.5;
-const s__FactoryFarm__data_waitBetweenBuilds = 0.1;
-const s__FactoryFarm__data_selectFarmType = FourCC( "A00M" );
+const WAIT_BETWEEN_TICKS = 2.5;
+const WAIT_BETWEEN_BUILDS = 0.1;
+const SELECT_FARM_TYPE = FourCC( "A00M" );
 
 const FARM_TYPE = FourCC( "hhou" );
 const BLACK_FARM_TYPE = FourCC( "h004" );
@@ -25,9 +20,12 @@ const GOLDEN_FARM_TYPE = FourCC( "h000" );
 const HARD_FARM_TYPE = FourCC( "hC06" );
 const TINY_FARM_TYPE = FourCC( "hC07" );
 
-// library FactoryFarm:
+type FactoryFarmData = {
+	buildIndex: number;
+	buildType: number;
+}
+const factoryFarmData: WeakMap<unit, FactoryFarmData> = new WeakMap();
 
-// todo: SpiralX/Y are broken on bottom right, jumps strangely
 const spiralX = ( n: number ): number => {
 
 	const k = Math.ceil( ( n ** 0.5 - 1 ) / 2 );
@@ -64,25 +62,22 @@ const spiralY = ( n: number ): number => {
 
 };
 
-let FactoryFarmStart: () => void = () => { /* do nothing */ };
+let factoryFarmStart: () => void = () => { /* do nothing */ };
 
-const FactoryFarm__FactoryFarmEnd = (): void => {
+const factoryFarmEnd = (): void => {
 
-	let wait = s__FactoryFarm__data_waitBetweenTicks - factoryFarmBuilds * s__FactoryFarm__data_waitBetweenBuilds;
+	let wait = WAIT_BETWEEN_TICKS - factoryFarmBuilds * WAIT_BETWEEN_BUILDS;
 
 	// Return sheep to unowned
 	SetUnitOwner( factoryFarmDummySheep, Player( PLAYER_NEUTRAL_PASSIVE ), false );
 
 	// Invoke next run
-
-	if ( wait < s__FactoryFarm__data_waitBetweenBuilds )
-		wait = s__FactoryFarm__data_waitBetweenBuilds;
-
-	TimerStart( factoryFarmTiemr, wait, false, FactoryFarmStart );
+	if ( wait < WAIT_BETWEEN_BUILDS ) wait = WAIT_BETWEEN_BUILDS;
+	TimerStart( factoryFarmTimer, wait, false, factoryFarmStart );
 
 };
 
-const FactoryFarm__FarmSize = ( farmType: number ): number => {
+const getFarmSize = ( farmType: number ): number => {
 
 	if ( farmType === HARD_FARM_TYPE ) return 320;
 	else if ( farmType === TINY_FARM_TYPE ) return 128;
@@ -90,46 +85,39 @@ const FactoryFarm__FarmSize = ( farmType: number ): number => {
 
 };
 
-const FactoryFarm__SpiralSize = ( size: number ): number => R2I( Pow( Math.floor( 640 / size ) * 2 + 1, 2 ) );
+const getSpiralSize = ( size: number ): number => R2I( Pow( Math.floor( 640 / size ) * 2 + 1, 2 ) );
 
-const FactoryFarm__FactoryFarmTick = (): void => {
+const factoryFarmTick = (): void => {
 
 	const u = FirstOfGroup( factoryFarmsTemp );
-	let farmType: number;
-	let spiralIndex: number;
-	let farmSize: number;
-	let x: number;
-	let y: number;
 
 	if ( u === null ) {
 
-		FactoryFarm__FactoryFarmEnd();
+		factoryFarmEnd();
 		return;
 
 	}
 
 	if ( UnitAlive( u ) ) {
 
-		farmType = LoadInteger( factoryFarmData, GetHandleId( u ), s__FactoryFarm__data_typeIndex );
-		farmSize = FactoryFarm__FarmSize( farmType );
+		const factoryData = factoryFarmData.get( u ) as FactoryFarmData;
+		const buildType = factoryData.buildType;
+		const farmSize = getFarmSize( buildType );
 
 		// Get next location
-		spiralIndex = LoadInteger( factoryFarmData, GetHandleId( u ), s__FactoryFarm__data_spiralIndex ) + 1;
+		let buildIndex = factoryData.buildIndex + 1;
+		if ( buildIndex > getSpiralSize( farmSize ) ) buildIndex = 2;
+		factoryFarmData.set( u, { ...factoryData, buildIndex } );
 
-		if ( spiralIndex > FactoryFarm__SpiralSize( farmSize ) )
-			spiralIndex = 2;
-
-		SaveInteger( factoryFarmData, GetHandleId( u ), s__FactoryFarm__data_spiralIndex, spiralIndex );
-
-		x = GetUnitX( u ) + Math.round( spiralX( spiralIndex ) ) * farmSize;
-		y = GetUnitY( u ) + Math.round( spiralY( spiralIndex ) ) * farmSize;
+		const x = GetUnitX( u ) + Math.round( spiralX( buildIndex ) ) * farmSize;
+		const y = GetUnitY( u ) + Math.round( spiralY( buildIndex ) ) * farmSize;
 
 		// Build the farm
 		SetUnitX( factoryFarmDummySheep, x );
 		SetUnitY( factoryFarmDummySheep, y );
 
 		SetUnitOwner( factoryFarmDummySheep, GetOwningPlayer( u ), false );
-		IssueBuildOrderById( factoryFarmDummySheep, farmType, x, y );
+		IssueBuildOrderById( factoryFarmDummySheep, buildType, x, y );
 
 		factoryFarmBuilds = factoryFarmBuilds + 1;
 
@@ -137,20 +125,20 @@ const FactoryFarm__FactoryFarmTick = (): void => {
 
 	GroupRemoveUnit( factoryFarmsTemp, u );
 
-	TimerStart( factoryFarmTiemr, s__FactoryFarm__data_waitBetweenBuilds, false, FactoryFarm__FactoryFarmTick );
+	TimerStart( factoryFarmTimer, WAIT_BETWEEN_BUILDS, false, factoryFarmTick );
 
 };
 
-FactoryFarmStart = (): void => {
+factoryFarmStart = (): void => {
 
 	factoryFarmBuilds = 0;
 	BlzGroupAddGroupFast( factoryFarms, factoryFarmsTemp );
 
-	FactoryFarm__FactoryFarmTick();
+	factoryFarmTick();
 
 };
 
-const FactoryFarm__GetBaseFarm = ( u: unit ): number => {
+const getBaseFarm = ( u: unit ): number => {
 
 	const playerId = GetPlayerId( GetOwningPlayer( u ) );
 
@@ -161,44 +149,36 @@ const FactoryFarm__GetBaseFarm = ( u: unit ): number => {
 
 };
 
-const FactoryFarm__FinishConstruction = (): void => {
+const onFinishConstruction = (): void => {
 
 	const u = GetTriggerUnit();
 
-	if ( GetUnitTypeId( u ) === FACTORY_FARM_TYPE ) {
+	if ( GetUnitTypeId( u ) !== FACTORY_FARM_TYPE ) return;
 
-		GroupAddUnit( factoryFarms, u );
-		SaveInteger( factoryFarmData, GetHandleId( u ), s__FactoryFarm__data_typeIndex, FactoryFarm__GetBaseFarm( u ) );
-		SaveInteger( factoryFarmData, GetHandleId( u ), s__FactoryFarm__data_spiralIndex, 1 );
-
-	}
+	GroupAddUnit( factoryFarms, u );
+	factoryFarmData.set( u, { buildIndex: 1, buildType: getBaseFarm( u ) } );
 
 };
 
-const FactoryFarm__SelectFarm = (): void => {
+const onSelectFarm = (): void => {
 
-	if ( GetSpellAbilityId() === s__FactoryFarm__data_selectFarmType )
-
-		SaveInteger( factoryFarmData, GetHandleId( GetTriggerUnit() ), s__FactoryFarm__data_typeIndex, GetUnitTypeId( GetSpellTargetUnit() ) );
+	if ( GetSpellAbilityId() === SELECT_FARM_TYPE )
+		factoryFarmData.set( GetTriggerUnit(), { ...factoryFarmData.get( GetTriggerUnit() ) as FactoryFarmData, buildType: GetUnitTypeId( GetSpellTargetUnit() ) } );
 
 };
 
-const FactoryFarm__Init = (): void => {
+addScriptHook( W3TS_HOOK.MAIN_AFTER, (): void => {
 
 	let t = CreateTrigger();
 	TriggerRegisterAnyUnitEventBJ( t, EVENT_PLAYER_UNIT_CONSTRUCT_FINISH );
-	TriggerAddAction( t, FactoryFarm__FinishConstruction );
+	TriggerAddAction( t, onFinishConstruction );
 
 	t = CreateTrigger();
 	TriggerRegisterAnyUnitEventBJ( t, EVENT_PLAYER_UNIT_SPELL_CAST );
-	TriggerAddAction( t, FactoryFarm__SelectFarm );
+	TriggerAddAction( t, onSelectFarm );
 
 	factoryFarmDummySheep = CreateUnit( Player( PLAYER_NEUTRAL_PASSIVE ), FourCC( "u002" ), 0, 0, 270 );
 
-	TimerStart( factoryFarmTiemr, s__FactoryFarm__data_waitBetweenTicks, false, FactoryFarmStart );
+	TimerStart( factoryFarmTimer, WAIT_BETWEEN_TICKS, false, factoryFarmStart );
 
-};
-
-// library FactoryFarm ends
-
-addScriptHook( W3TS_HOOK.MAIN_AFTER, FactoryFarm__Init );
+} );
