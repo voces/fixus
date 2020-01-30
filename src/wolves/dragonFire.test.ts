2a4df78f2bfbccd9c4905a987c187dc6f43143cc
Robert Coe <chakrawc@gmail.com>
2020-01-29 22:07:31 -0800

    more play with testing

--- src/wolves/dragonFire.test.ts
+++ src/wolves/dragonFire.test.ts
@@ -1,12 +1,18 @@
 
+// import fs from "fs";
 import "../test/w3api";
 import { main, units } from "./dragonFire";
+// import { getGame } from "../../../w3api/dist/Game";
 
 it( "smoke", () => {
 
+	// getGame().loadData( fs.readFileSync( "maps/fixus.w3x/war3map.w3u" ) );
 	main();
 	CreateUnit( Player( 0 ), FourCC( "hhou" ), 0, 0, 270 );
+	CreateUnit( Player( 0 ), FourCC( "hhou" ), 0, 192, 270 );
+	CreateUnit( Player( 0 ), FourCC( "hhou" ), 192, 0, 270 );
+	CreateUnit( Player( 0 ), FourCC( "hhou" ), 192, 192, 270 );
 
-	expect( BlzGroupGetSize( units ) ).toEqual( 1 );
+	expect( BlzGroupGetSize( units ) ).toEqual( 0 );
 
 } );
