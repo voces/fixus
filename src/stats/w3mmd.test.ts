35372fca1673da29bb4e9eb3f25055b7a2e33b9a
Robert Coe <chakrawc@gmail.com>
2020-03-31 09:49:34 -0700

    remove debugger statement

--- src/stats/w3mmd.test.ts
+++ src/stats/w3mmd.test.ts
@@ -4,13 +4,8 @@ import { pack } from "./w3mmd";
 
 describe( "pack", () => {
 
-	it( "smoke", () => {
-
-		debugger;
-		expect(
-			pack( "hello world how are \"you\" this fine \"morning? 3\\2 = 1.5, and 4\\2 = 2" ),
-		).toEqual( "hello\\ world\\ how\\ are\\ \"you\"\\ this\\ fine\\ \"morning?\\ 3\\\\2\\ =\\ 1.5,\\ and\\ 4\\\\2\\ =\\ 2" );
-
-	} );
+	it( "smoke", () => expect(
+		pack( "hello world how are \"you\" this fine \"morning? 3\\2 = 1.5, and 4\\2 = 2" ),
+	).toEqual( "hello\\ world\\ how\\ are\\ \"you\"\\ this\\ fine\\ \"morning?\\ 3\\\\2\\ =\\ 1.5,\\ and\\ 4\\\\2\\ =\\ 2" ) );
 
 } );
