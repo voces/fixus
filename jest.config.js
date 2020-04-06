2af3e7854326e275176c147d76fbb84cff63553c
Robert Coe <chakrawc@gmail.com>
2020-04-05 17:48:09 -0700

    fix building + tests

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
 	globals: { "ts-jest": { tsConfig: "tsconfig.test.json" } },
 	moduleDirectories: [ "node_modules", "src" ],
 	moduleFileExtensions: [ "ts", "js" ],
+	moduleNameMapper: {
+		"^test/(.*)$": "<rootDir>/test/$1",
+	},
 	transform: {
 		"^.+\\.ts$": "ts-jest",
 		"^.+\\.js$": "babel-jest",
