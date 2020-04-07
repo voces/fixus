49ab624e07cb36736157ef98be641187ddddda69
Robert Coe <chakrawc@gmail.com>
2020-04-06 19:04:01 -0700

    fix zoom and remove todo

--- src/commands/zoom.ts
+++ src/commands/zoom.ts
@@ -1,36 +1,78 @@
 
-import { addScriptHook, W3TS_HOOK } from "@voces/w3ts";
-import { wolfTeam } from "shared";
-import { closeFile, writeFile, openFile, readAndCloseFile } from "util/fileIO";
+import { addScriptHook, W3TS_HOOK, File } from "@voces/w3ts";
+import { wolfTeam, sheepTeam, wispTeam } from "shared";
 import { registerCommand } from "./registerCommand";
 
-const sheepZoom: Array<number> = [];
-const wolfZoom: Array<number> = [];
+let sheepZoom = 1650;
+let wolfZoom = 1650;
 
 // ===========================================================================
 // Trigger: miscZoom
 // ===========================================================================
 
-const action = ( { zoom = 0 }: {zoom: number} ): void => {
+const adjustZoom = ( zoom: number ): number => {
 
-	const playerId = GetPlayerId( GetTriggerPlayer() );
+	const isWolf = IsPlayerInForce( GetLocalPlayer(), wolfTeam );
 
+	// A zoom was not passed, pull from palyer default
 	if ( zoom === 0 )
-		zoom = IsPlayerInForce( GetTriggerPlayer(), wolfTeam ) ?
-			wolfZoom[ playerId ] :
-			sheepZoom[ playerId ];
+		zoom = isWolf ?
+			wolfZoom :
+			sheepZoom;
 
+	// No player default, use constant default
+	if ( zoom === 0 )
+		zoom = 1650;
+
+	// An invalid zoom was passed, use constant default
 	if ( zoom <= 0 ) zoom = 1650;
 
+	// A short zoom was passed, so lengthen (23 -> 230 -> 2300)
 	while ( zoom <= 400 ) zoom *= 10;
 
-	if ( IsPlayerInForce( GetTriggerPlayer(), wolfTeam ) ) wolfZoom[ playerId ] = zoom;
-	else sheepZoom[ playerId ] = zoom;
+	return zoom;
 
-	if ( GetLocalPlayer() === GetTriggerPlayer() ) {
+};
 
+export const zoom = ( zoom?: number ): void => {
+
+	const player = GetTriggerPlayer();
+	const isWolf = IsPlayerInForce( player, wolfTeam );
+	const isSheep = IsPlayerInForce( player, sheepTeam ) || IsPlayerInForce( player, wispTeam );
+
+	zoom = adjustZoom( zoom || 0 );
+
+	if ( GetLocalPlayer() === player ) {
+
+		// Zoom out
 		SetCameraField( CAMERA_FIELD_TARGET_DISTANCE, zoom, 0 );
-		closeFile( writeFile( openFile( "fixus/zooms.txt" ), R2S( sheepZoom[ playerId ] ) + " " + R2S( wolfZoom[ playerId ] ) ) );
+
+		// Update local value
+		let changed = false;
+		if ( isWolf || isSheep )
+			if ( isWolf ) {
+
+				if ( wolfZoom !== zoom ) {
+
+					wolfZoom = zoom;
+					changed = true;
+
+				}
+
+			} else {
+
+				if ( sheepZoom !== zoom ) {
+
+					sheepZoom = zoom;
+					changed = true;
+
+				}
+
+			}
+
+		// Update persisted value
+		if ( changed )
+			File.write( "fixus/zooms.txt", `${sheepZoom} ${wolfZoom}` );
 
 	}
 
@@ -43,26 +85,17 @@ registerCommand( {
 	alias: "z",
 	description: "Sets the camera zoom to the passed amount.",
 	args: [ { name: "zoom", type: "number", required: false } ],
-	fn: action,
+	fn: ( { zoom: zoomArg }: {zoom: number} ) => zoom( zoomArg ),
 } );
 
 addScriptHook( W3TS_HOOK.MAIN_AFTER, (): void => {
 
-	const zooms = ( readAndCloseFile( openFile( "fixus/zooms.txt" ) ) || "" ).split( " " );
-	const playerId = GetPlayerId( GetLocalPlayer() );
+	const zooms = File.read( "fixus/zooms.txt" ).replace( "fail", "" ).split( " " );
 
-	sheepZoom[ playerId ] = S2R( zooms[ 0 ] || "" );
-	if ( sheepZoom[ playerId ] === 0 )
-		sheepZoom[ playerId ] = 1650;
+	sheepZoom = adjustZoom( S2R( zooms[ 0 ] || "" ) );
+	wolfZoom = adjustZoom( S2R( zooms[ 1 ] || "" ) );
 
-	wolfZoom[ playerId ] = S2R( zooms[ 1 ] || "" );
-	if ( wolfZoom[ playerId ] === 0 )
-		wolfZoom[ playerId ] = 1650;
-
-	SetCameraField(
-		CAMERA_FIELD_TARGET_DISTANCE,
-		IsPlayerInForce( GetLocalPlayer(), wolfTeam ) ? wolfZoom[ playerId ] : sheepZoom[ playerId ],
-		0,
-	);
+	// Zoom to sheep zoome for now
+	SetCameraField( CAMERA_FIELD_TARGET_DISTANCE, sheepZoom, 0 );
 
 } );
